6618d0f43e77a6adbb23800fa855105ad14198a6
Joan Sala <contact@joansala.com>
2025-08-18 17:58:23 +0200

    Fix benchmark suite path for tests

--- src/test/java/com/joansala/test/game/go/GoBoardTest.java
+++ src/test/java/com/joansala/test/game/go/GoBoardTest.java
@@ -13,7 +13,7 @@ import com.joansala.util.suites.SuiteReader;
 public class GoBoardTest implements BoardContract {
 
     /** Test suite file path */
-    private static String SUITE_PATH = "go-bench.suite";
+    private static String SUITE_PATH = "src/test/resources/go-bench.suite";
 
 
     /**
